8151993daf21db5368ac38afe96ed3f796b9f068
simon <simon@student-net-etx-2334.intern.ethz.ch>
2023-10-30 10:57:28 +0100

    DP classes hinzugefügt

---
+++ Theory_Algorithms/src/DP/MED.java
@@ -0,0 +1,5 @@
+package DP;
+
+public class MED { // minimal Editing distance
+
+}
--- Theory_Algorithms/src/Datastructures/SearchTree.java
+++ Theory_Algorithms/src/Datastructures/SearchTree.java
@@ -3,6 +3,14 @@ package Datastructures;
 import AbstractDataTypes.*;
 
 public class SearchTree<T> implements Dictionnary<T>{
+	
+	Node<T> head;
+	Node<T>[] ref; // ind. 0 : left & ind. 1 : right
+	
+	public SearchTree() {
+		this.head = null;
+		this.ref = null;
+	}
 
 	@Override
 	public void insert(T key) throws IllegalArgumentException {
